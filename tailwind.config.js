764ffff63635e95a8ab2e512e4bb23e5535b75fa
Justin Shaw <31273938+neokn@users.noreply.github.com>
2025-12-03 07:48:13 +0800

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,svelte}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,svelte}',
+    './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [require('flowbite/plugin')],
 };
