791d6450640c44f03d53ac33a9bbbc947e72df65
Neo Kusanagi <jr@neo.kn>
2025-12-03 08:21:56 +0800

    feat: add privacy policy page and configure Vite to copy static assets.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'privacy_policy.html',
+          dest: '.'
+        }
+      ]
+    })
+  ],
 });
